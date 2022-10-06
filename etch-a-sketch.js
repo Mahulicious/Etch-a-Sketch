2f68501ad692da844e2911f0ae5b29218c63872a
Mahulicious <maheshdhimal61@gmail.com>
2022-10-06 00:55:52 -0400

    Added eventlistener that draws on sketch pad

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,27 @@
 // Creating 256 divs to use as 16x16 divs
 
+
 for (let i = 0; i < 256; i++ ) {
     let gridDiv = document.createElement('div');
     gridDiv.className = "grids"
     let containerDiv = document.querySelector('.container');
     containerDiv.append(gridDiv);
 }
+
+
+
+
+
+//Set up the hover effect 
+const draws = document.querySelectorAll('.grids');
+for (i=0; i < draws.length; i++) {
+    
+    draws[i].addEventListener('mouseenter', function() {
+    this.style.backgroundColor = "gray"
+});
+}
+
+
+
+
+
